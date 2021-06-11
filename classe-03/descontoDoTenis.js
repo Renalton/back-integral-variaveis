b57eb51351b8ab71e13d67bfe84c2fe0fa8f2bbe
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:06:58 -0300

    Calcular o desconto em porcentagem gerado por um cupom

---
+++ classe-03/descontoDoTenis.js
@@ -0,0 +1,8 @@
+//Execicío para calcular o desconto gerado por um cupom através da regra de três
+// Programação do Zero Cubos Academy e Ifood
+
+let desconto = 0, dinheiro = 80.00, tenis = 129.99;
+
+desconto = (dinheiro * 100) / tenis;
+console.log(`Seu desconto foi de ${desconto}%`);
+
