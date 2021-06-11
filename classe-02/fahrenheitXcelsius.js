a7d157d631ce59325f67674ea46e8293b0270289
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:04:19 -0300

    Código para converter graus em Fahrenheit para graus em Celsius

---
+++ classe-02/fahrenheitXcelsius.js
@@ -0,0 +1,8 @@
+//Código para converter graus em Fahrenheit para graus em Celsius
+// Programação do Zero Cubos Academy e Ifood
+
+let celsius = 0, fheit = 73;
+
+celsius = (fheit - 32) * 5/9;
+
+console.log(celsius);
