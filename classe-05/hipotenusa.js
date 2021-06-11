bd4eccac39d63ecf9786e954c82cbd08f07123ac
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:07:54 -0300

    Calcule a distância entre dois pontos

---
+++ classe-05/hipotenusa.js
@@ -0,0 +1,7 @@
+//Calcule a distância entre dois pontos
+// Programação do Zero Cubos Academy e Ifood
+
+let distancia = 0, x1 = 1, x2 = 1, y1 = 1, y2 = 4;
+
+distancia = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2 - y1), 2));
+console.log(distancia);
