51732d53246550c9f0b2d278ccfd39962a7ccf32
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:01:28 -0300

    Simulação calcular a quantidade de infectados pelo COVID-19

---
+++ casa-07/casosCovidDezenove.js
@@ -0,0 +1,7 @@
+// Código para calcular a quantidade de pessoas infectadas pelo vírus a partir de 3 variáveis
+// Programação do Zero Cubos Academy e Ifood
+
+let jaInfectados = 1000, seraInfectado = 4, tempo = 100;
+
+totalInfectados = jaInfectados * Math.pow(seraInfectado, (tempo / 7));
+console.log(totalInfectados); 
