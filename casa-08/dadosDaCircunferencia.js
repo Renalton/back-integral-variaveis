94511c0ac1ebba9c8529f9c5c0e1e48b5b4d7ade
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:01:59 -0300

    Código para determinar o comprimento e a área do circulo

---
+++ casa-08/dadosDaCircunferencia.js
@@ -0,0 +1,11 @@
+// Código para determinar o comprimento e a área do circulo
+//Programação do Zero Cubos Academy e Ifood
+
+//Para esse código vou considerar pi = 3.14159
+
+let comprimento = 0, pi = 3.14159, raio = 5, area = 10;
+
+comprimento = (2 * pi * raio);
+area = (pi * Math.pow(raio, 2));
+
+console.log(`O comprimento do circulo é ${comprimento}} e a área é de ${area}`);
