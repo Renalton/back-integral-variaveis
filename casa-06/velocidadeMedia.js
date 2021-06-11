c030185a3554eb246945d6ac5bdff7c184d577d7
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 04:59:22 -0300

    Código para calcular a velocidade média

---
+++ casa-06/velocidadeMedia.js
@@ -0,0 +1,7 @@
+//Código para calcular a velocidade média
+// Programação do Zero Cubos Academy e Ifood
+
+let velocidade = 0, distancia = 100, tempo = 10;
+
+velocidade = (distancia / tempo) * 3.6;
+console.log(velocidade);
