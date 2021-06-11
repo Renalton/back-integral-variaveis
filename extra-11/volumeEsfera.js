964f3bfd93ede2f94456d796d9c51a12cc3067f7
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:08:28 -0300

    Código para determinar o volume de uma esfera

---
+++ extra-11/volumeEsfera.js
@@ -0,0 +1,8 @@
+//Código para determinar o volume de uma esfera
+//Programação do Zero Cubos Academy e Ifood
+
+//Para esse código vou considerar pi = 3,14159
+let volume = 0, raio = 3, pi = 3.14159;
+
+volume = (4 * pi * (Math.pow(raio, 3) ) ) / 3;
+console.log(`O volume de uma esfera de raio 3 é ${volume}L`);
