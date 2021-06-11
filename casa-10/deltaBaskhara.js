ec1aadf6323c93c4eed96df57cec2c22055b766f
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:02:56 -0300

    Código para calcular o delta da fórmula de Báskhara

---
+++ casa-10/deltaBaskhara.js
@@ -0,0 +1,7 @@
+//Código para calcular o delta da fórmula de Báskhara
+//Programação do Zero Cubos Academy e Ifood
+
+let delta  = 0, nA = 0, nB = 3, nC = 0;
+
+delta = Math.pow(nB, 2) - 4 * nA * nC;
+console.log(delta);
