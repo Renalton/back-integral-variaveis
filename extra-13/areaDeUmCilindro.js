c0776f03acd1ab4924c5a61ae27078c170c68733
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-11 05:09:26 -0300

    Código para calcular a área de um cilídro

---
+++ extra-13/areaDeUmCilindro.js
@@ -0,0 +1,8 @@
+//Código para calcular a área de um cilídro
+//Programação do Zero Cubos Academy e Ifood
+
+//Para esse código vou considerar pi = 3,14159
+let aTotal = 0, aBase = 0, aLateral = 0, pi = 3.14159, raio = 5, altura = 16;
+
+aTotal = 2*pi*raio*(raio + altura);
+console.log(aTotal);
